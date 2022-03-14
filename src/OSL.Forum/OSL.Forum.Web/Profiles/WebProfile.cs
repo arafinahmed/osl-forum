e67e7bd254a814def49a08e3a2d1b5bafeef3967
Arafin Ahmed <arafin2021@gmail.com>
2022-03-14 17:37:16 +0600

    Auto mapper configuration.

---
+++ src/OSL.Forum/OSL.Forum.Web/Profiles/WebProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OSL.Forum.Web.Profiles
+{
+    public class WebProfile : Profile
+    {
+        public WebProfile()
+        {
+
+        }
+    }
+}
--- src/OSL.Forum/OSL.Forum.Web/WebModule.cs
+++ src/OSL.Forum/OSL.Forum.Web/WebModule.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using AutoMapper;
+using OSL.Forum.Web.Profiles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,23 @@ namespace OSL.Forum.Web
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.Register(context => new MapperConfiguration(cfg =>
+            {
+                //Register Mapper Profile
+                cfg.AddProfile<WebProfile>();
+            }
+            )).AsSelf().InstancePerRequest();
+
+            builder.Register(c =>
+            {
+                //This resolves a new context that can be used later.
+                var context = c.Resolve<IComponentContext>();
+                var config = context.Resolve<MapperConfiguration>();
+                return config.CreateMapper(context.Resolve);
+            })
+                .As<IMapper>()
+                .InstancePerLifetimeScope();
+
             base.Load(builder);
         }
     }

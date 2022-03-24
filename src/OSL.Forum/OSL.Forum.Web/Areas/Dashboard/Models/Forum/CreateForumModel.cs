1e27b6d53bd9d31013c511c5c453941202a49edb
Arafin Ahmed <arafin2021@gmail.com>
2022-03-24 18:05:15 +0600

    Forum model done.

--- src/OSL.Forum/OSL.Forum.Web/Areas/Dashboard/Models/Forum/CreateForumModel.cs
+++ src/OSL.Forum/OSL.Forum.Web/Areas/Dashboard/Models/Forum/CreateForumModel.cs
@@ -2,7 +2,11 @@
 using AutoMapper;
 using OSL.Forum.Core.Services;
 using OSL.Forum.Core.Utilities;
+using OSL.Forum.Web.Services;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 using BO = OSL.Forum.Core.BusinessObjects;
 
 namespace OSL.Forum.Web.Areas.Dashboard.Models.Forum
@@ -13,21 +17,25 @@ namespace OSL.Forum.Web.Areas.Dashboard.Models.Forum
         [Display(Name = "Forum Name")]
         [StringLength(64, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
         public string Name { get; set; }
+        [Required]
+        public Guid CategoryId { get; set; }
         private ILifetimeScope _scope;
         private IForumService _forumService;
         private IMapper _mapper;
         private IDateTimeUtility _dateTimeUtility;
+        private IProfileService _profileService;
 
         public CreateForumModel()
         {
         }
 
         public CreateForumModel(IForumService forumService,
-            IMapper mapper, IDateTimeUtility dateTimeUtility)
+            IMapper mapper, IDateTimeUtility dateTimeUtility, IProfileService profileService)
         {
             _forumService = forumService;
             _mapper = mapper;
             _dateTimeUtility = dateTimeUtility;
+            _profileService = profileService;
         }
 
         public void Resolve(ILifetimeScope scope)
@@ -36,6 +44,32 @@ namespace OSL.Forum.Web.Areas.Dashboard.Models.Forum
             _forumService = _scope.Resolve<IForumService>();
             _mapper = _scope.Resolve<IMapper>();
             _dateTimeUtility = _scope.Resolve<IDateTimeUtility>();
+            _profileService = _scope.Resolve<IProfileService>();
+        }
+
+        public async Task Create()
+        {
+            var user = _profileService.GetUser();
+
+            if (user == null)
+                throw new InvalidOperationException("No user found for create a forum");
+
+            var roles = await _profileService.UserRoles();
+            
+            if(!roles.Contains("SuperAdmin") && !roles.Contains("Admin"))
+                throw new InvalidOperationException("You are not allowed to create a forum");
+
+            var time = _dateTimeUtility.Now;
+            var forum = new BO.Forum()
+            {
+                Name = Name,
+                CategoryId = CategoryId,
+                ApplicationUserId = user.Id,
+                CreationDate = time,
+                ModificationDate = time,
+            };
+
+            _forumService.CreateForum(forum);
         }
     }
 }

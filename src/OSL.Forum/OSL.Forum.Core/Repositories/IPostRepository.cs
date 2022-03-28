2da165f4aa557770176db38052828bfe4367ed03
Arafin Ahmed <arafin2021@gmail.com>
2022-03-28 10:09:24 +0600

    post and topic repository created.

---
+++ src/OSL.Forum/OSL.Forum.Core/Repositories/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using EO = OSL.Forum.Core.Entities;
+using OSL.Forum.DataAccessLayer.Repositories;
+using System;
+
+
+namespace OSL.Forum.Core.Repositories
+{
+    public interface IPostRepository : IRepository<EO.Post, Guid>
+    {
+    }
+}
---
+++ src/OSL.Forum/OSL.Forum.Core/Repositories/TopicRepository.cs
@@ -0,0 +1,15 @@
+﻿using OSL.Forum.Core.Contexts;
+using EO = OSL.Forum.Core.Entities;
+using OSL.Forum.DataAccessLayer.Repositories;
+using System;
+
+namespace OSL.Forum.Core.Repositories
+{
+    public class TopicRepository : Repository<EO.Topic, Guid>, ITopicRepository
+    {
+        public TopicRepository(ICoreDbContext dbContext)
+            : base((CoreDbContext)dbContext)
+        {
+        }
+    }
+}

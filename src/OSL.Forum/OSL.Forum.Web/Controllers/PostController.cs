3300104fc9de9f2fa560c1089e9794dec03fbfa5
Arafin Ahmed <arafin2021@gmail.com>
2022-03-30 08:59:44 +0600

    Post controller created.

---
+++ src/OSL.Forum/OSL.Forum.Web/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using log4net;
+using OSL.Forum.Web.Models.Category;
+using OSL.Forum.Web.Models.Forum;
+using OSL.Forum.Web.Models.Topic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSL.Forum.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(PostController));
+        private readonly ILifetimeScope _scope;
+
+        public PostController(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+    }
+}

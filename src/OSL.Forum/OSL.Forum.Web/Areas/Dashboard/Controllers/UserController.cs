22cbfa94ccec0c91713f79c01d9e274c32beace6
Arafin Ahmed <arafin2021@gmail.com>
2022-04-03 10:31:38 +0600

    UserController created.

---
+++ src/OSL.Forum/OSL.Forum.Web/Areas/Dashboard/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using log4net;
+using OSL.Forum.Web.Areas.Dashboard.Models.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSL.Forum.Web.Areas.Dashboard.Controllers
+{
+    [Authorize(Roles ="SuperAdmin, Admin")]
+    public class UserController : Controller
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(UserController));
+        private readonly ILifetimeScope _scope;
+
+        public UserController(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+    }
+}

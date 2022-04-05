170ccd8db27fbfcd70da13007a6a44cbbc215b9d
Arafin Ahmed <arafin2021@gmail.com>
2022-04-05 14:41:31 +0600

    Controller & View created.

---
+++ src/OSL.Forum/OSL.Forum.Web/Areas/Dashboard/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using log4net;
+using OSL.Forum.Web.Areas.Dashboard.Models.AssignRole;
+using OSL.Forum.Web.Areas.Dashboard.Models.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSL.Forum.Web.Areas.Dashboard.Controllers
+{
+    [Authorize(Roles = "SuperAdmin, Admin, Moderator")]
+    public class PostController : Controller
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(PostController));
+        private readonly ILifetimeScope _scope;
+
+        public PostController(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+        public ActionResult Pending()
+        {
+            return View();
+        }
+    }
+}
